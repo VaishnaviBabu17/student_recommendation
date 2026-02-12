fa430584ea74773ebf94115c0118e377055327c7
VaishnaviBabu17 <merlinbabu@gmail.com>
2026-02-12 14:05:13 +0530

    added create admin url

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your tests here.
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin123'
+        )
+    return HttpResponse("Superuser created")
--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('create-admin/', views.create_admin),
 ]
