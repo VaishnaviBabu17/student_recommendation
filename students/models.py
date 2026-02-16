0ddfb099ba48427ea0ee8929a4f82ab17cbdd1e8
VaishnaviBabu17 <merlinbabu@gmail.com>
2026-02-16 09:47:44 +0530

    Reset database and updated Student model

--- students/models.py
+++ students/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Student(models.Model):
+    reg_no = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=50)
+    year = models.IntegerField()
     semester = models.IntegerField()
 
     total_classes = models.IntegerField()
@@ -12,3 +14,6 @@ class Student(models.Model):
         if self.total_classes == 0:
             return 0
         return (self.classes_attended / self.total_classes) * 100
+
+    def __str__(self):
+        return f"{self.name} ({self.reg_no})"
