00063d3c65f4d7ca47f70f02cc2d8bf7da921f11
VaishnaviBabu17 <merlinbabu@gmail.com>
2026-02-24 11:48:22 +0530

    fix: strip whitespace from ALLOWED_HOSTS

--- student_recommendation/settings.py
+++ student_recommendation/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-later')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')]
 
 # CSRF Settings for Production
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
