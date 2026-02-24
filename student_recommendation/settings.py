c234c86090a106d108f8be7cf498fdec2675722c
VaishnaviBabu17 <merlinbabu@gmail.com>
2026-02-24 14:31:20 +0530

    add PostgreSQL support

--- student_recommendation/settings.py
+++ student_recommendation/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,10 +71,10 @@ WSGI_APPLICATION = 'student_recommendation.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
