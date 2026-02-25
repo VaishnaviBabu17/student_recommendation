7476fd0a43800323ad1ebb6a8adcaa0367db7506
VaishnaviBabu17 <merlinbabu@gmail.com>
2026-02-25 10:14:59 +0530

    Auto-populate roll_no in admin dashboard

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Student
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'roll_no', 'department', 'year', 'semester']
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.roll_no and obj.user:
+            obj.roll_no = obj.user.username
+        super().save_model(request, obj, form, change)
